df2e816cd6e0c0a806ef16d773dcff8e1c55130c
n4ught1us-max1mus <n4ught1us.max1mus@gmail.com>
2018-11-26 22:57:48 +0000

    progress on the gui; started working on importing files

--- gui.py
+++ gui.py
@@ -12,11 +12,25 @@ class StartPage(Frame):
         r1.pack(anchor=W)
         r2 = Radiobutton(self, text="Import participants from a txt file", variable=opt, value=2)
         r2.pack(anchor=W)
-        r3 = Radiobutton(self, text="Exit", variable=opt, value=3)
+        r3 = Radiobutton(self, text="Leave feedback", variable=opt, value=3)
         r3.pack(anchor=W)
-        btn = Button(self, text="Continue", command=lambda: command(opt), width=10)
+        btn = Button(self, text="Continue", command=lambda: self.command(opt), width=10)
         btn.pack(side=BOTTOM, anchor=E, pady=5, padx=5)
 
+    def command(self, i):
+        print("option selected is " + str(i.get()))
+        if i.get() == 1:
+            raise_frame(wip)
+
+        elif i.get() == 2:
+            raise_frame(ip)
+
+        elif i.get() == 3:
+            # TODO Open feedback page and send email to myself
+            raise_frame(wip)
+
+        else:
+            print("The option is " + i.get())
 
 class ImportParticipants(Frame):
 
@@ -24,13 +38,40 @@ class ImportParticipants(Frame):
         Frame.__init__(self, master)
         self.grid(sticky=NSEW)
         Label(self, text="To import participants, please put the file in the same directory as the app.")\
-            .pack(fill=X, expand=TRUE)
+            .pack(fill=X)
+        lbl = Label(self, text="")
+        lbl.pack(fill=X, expand=TRUE)
         Label(self, text="Enter the file name: ").pack(side=LEFT, fill=BOTH, expand=TRUE)
         e = Entry(self)
         e.pack(side=LEFT, pady=5, padx=5, expand=TRUE, fill=X)
-        btn = Button(self, text="Continue", command=lambda: command(e), width=10)
+        btn = Button(self, text="Continue", command=lambda: self.command(e, lbl), width=10)
         btn.pack(side=BOTTOM, anchor=E, pady=5, padx=5)
 
+    def command(self, i, a):
+        import os
+        file_path = os.path.join(os.getcwd(), str(i.get()))
+        if not os.path.exists(file_path):
+            a.configure(text="File cannot be found!\nImport the file and try again.")
+        else:
+            a.configure(text="File found!")
+
+
+class Navigation(Frame):
+
+    def __init__(self, master):
+        Frame.__init__(self, master, bd=1, relief=RAISED)
+        self.grid(sticky=NSEW)
+        btn = Button(self, text="Home", command=lambda: self.home(), width=10)
+        btn.pack(side=LEFT, anchor=E, pady=5, padx=5)
+        btn = Button(self, text="Exit", command=lambda: self.close(), width=10)
+        btn.pack(side=LEFT, anchor=E, pady=5, padx=5)
+        Label(self, text="v0.1 Yordan Angelov © Copyright 2018").pack(side=RIGHT, padx=5)
+
+    def home(self):
+        raise_frame(s)
+
+    def close(self):
+        exit()
 
 class WorkInProgress(Frame):
 
@@ -44,20 +85,6 @@ if __name__ == '__main__':
     def raise_frame(frame):
         frame.tkraise()
 
-    def command(i):
-        print("option selected is " + str(i.get()))
-        if i.get() == 1:
-            raise_frame(wip)
-
-        elif i.get() == 2:
-            raise_frame(ip)
-
-        elif i.get() == 3:
-            exit()
-
-        else:
-            print("The option is " + i.get())
-
     root = Tk()
     opt = IntVar()
     root.title("Secret Santa")
@@ -70,21 +97,12 @@ if __name__ == '__main__':
     s = StartPage(root)
     ip = ImportParticipants(root)
     wip = WorkInProgress(root)
+    nav = Navigation(root)
 
     for frame in (s, ip, wip):
         frame.grid(row=0, column=0, sticky='news')
 
-    # Button(f1, text='Go to frame 2', command=lambda: raise_frame(f2)).pack()
-    # Label(f1, text='FRAME 1').pack()
-    #
-    # Label(f2, text='FRAME 2').pack()
-    # Button(f2, text='Go to frame 3', command=lambda: raise_frame(f3)).pack()
-    #
-    # Label(f3, text='FRAME 3').pack()
-    # Button(f3, text='Go to frame 4', command=lambda: raise_frame(f4)).pack()
-    #
-    # Label(f4, text='FRAME 4').pack()
-    # Button(f4, text='Goto to frame 1', command=lambda: raise_frame(s)).pack()
+    nav.grid(row=1, column=0, sticky='news')
 
     raise_frame(s)
 
